781e64e3d1d493c1919d984cc0ed8a16d8bf3e1b
tancos02 <37735433+tancos02@users.noreply.github.com>
2019-03-20 10:19:45 +0700

    Update Cell.h

--- Cell/Cell.h
+++ Cell/Cell.h
@@ -3,17 +3,12 @@
 #define CELL_H
 
 class Cell {
-protected:  // Posisi dari cell dalam bentuk (x,y)
-    int x;
-    int y;
 public:
     Cell();  // CTOR tanpa parameter
     Cell(int,int);  // CTOR dengan parameter
     // Getter dan setter dari value
     void setX(int);
     void setY(int);
-    int getX();
-    int getY();
 };
 
 #endif
