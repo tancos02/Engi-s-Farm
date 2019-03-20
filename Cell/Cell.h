b376867fec9ac461702665a36dfed8138fb6002c
tancos02 <37735433+tancos02@users.noreply.github.com>
2019-03-20 10:25:13 +0700

    Cell.h update
    updating Cell.h. become a derived class for renderable and erasing some attributes and methods

--- Cell/Cell.h
+++ Cell/Cell.h
@@ -2,18 +2,15 @@
 #ifndef CELL_H
 #define CELL_H
 
+#include "Renderable.h"
+
 class Cell {
-protected:  // Posisi dari cell dalam bentuk (x,y)
-    int x;
-    int y;
 public:
     Cell();  // CTOR tanpa parameter
     Cell(int,int);  // CTOR dengan parameter
     // Getter dan setter dari value
     void setX(int);
     void setY(int);
-    int getX();
-    int getY();
 };
 
 #endif
